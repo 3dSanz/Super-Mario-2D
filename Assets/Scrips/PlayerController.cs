4304a5af6da1fc389182dce6831ccd9b27181036
3dSanz <danisanzpelegrin@gmail.com>
2023-01-18 10:54:16 +0100

    Nivel 1 acabado
    18/01/2023

--- Assets/Scrips/PlayerController.cs
+++ Assets/Scrips/PlayerController.cs
@@ -7,10 +7,17 @@ public class PlayerController : MonoBehaviour
     int playerHealth = 3;
     public float playerSpeed = 5.5f;
     string texto = "Hello World";
-    bool isGrounded = false;
+    private SpriteRenderer spriteRenderer;
+    float horizontal;
+    private Rigidbody2D rBody; 
+    public float jumpForce = 3f;
+    private GroundSensor sensor;
     // Start is called before the first frame update
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rBody = GetComponent<Rigidbody2D>();
+        sensor = GameObject.Find("GroundSensor").GetComponent<GroundSensor>();
         playerHealth = 10;
         Debug.Log(texto);
     }
@@ -18,6 +25,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, 0) * playerSpeed * Time.deltaTime;
+        horizontal = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(horizontal, 0, 0) * playerSpeed * Time.deltaTime;
+        if (horizontal < 0)
+            {
+                spriteRenderer .flipX = true;
+            } else if (horizontal > 0)
+                {
+                    spriteRenderer .flipX = false;
+                }
+        if (Input.GetButtonDown("Jump") && sensor.isGrounded)
+            {
+                rBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
+
     }
 }
